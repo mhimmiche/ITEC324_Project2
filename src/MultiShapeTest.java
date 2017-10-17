7b2c7de033bf055324e3a175bb3f74553ce7134d
Mehdi Himmiche <mhimmiche@radford.edu>
2017-10-16 21:40:07 -0400

    Initial upload of files

---
+++ src/MultiShapeTest.java
@@ -0,0 +1,10 @@
+public class MultiShapeTest {
+    /**
+     * Instantiates the frame
+     * @param args
+     */
+    public static void main(String[] args) {
+        CarFrame cf = new CarFrame();
+        cf.carFrame();
+    }
+}
